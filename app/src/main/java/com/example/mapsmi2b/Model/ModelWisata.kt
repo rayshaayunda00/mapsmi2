a58cb00e16128857b420c6f7799a99c283f96f15
unknown <rayshaayuda@gmail.com>
2024-11-04 11:01:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mapsmi2b/Model/ModelWisata.kt
@@ -0,0 +1,10 @@
+package com.example.mapsmi2b.Model
+
+class ModelWisata (
+    var image : Int,
+    var nama : String,
+    var daerah : String,
+    var deskripsi : String,
+    val latitude: Double,
+    val longitude: Double
+)
